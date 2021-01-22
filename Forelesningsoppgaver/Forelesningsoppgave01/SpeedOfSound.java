df5c3b43d2e99fcf3f5ff349dff5a8073f29848e
Lars Emil Knudsen <larseknu@hiof.no>
2021-01-22 13:17:32 +0100

    Løsningsforslag til forelesningsoppgave 01

---
+++ Forelesningsoppgaver/Forelesningsoppgave01/SpeedOfSound.java
@@ -0,0 +1,12 @@
+public class SpeedOfSound {
+
+    public static void main(String[] args) {
+        int speedOfSound = 330;
+        double time = 6.9;
+
+        double distance = speedOfSound * time;
+
+        System.out.println("Lynet er " + distance + " meter unna.");
+    }
+
+}
